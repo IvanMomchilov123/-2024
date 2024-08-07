8de50b90e58e2b0ca153d869478f6c06e7e13c8d
IvanMomchilov123 <vanko.momchilov@gmail.com>
2024-08-07 13:49:59 +0300

    Added Users and Trips in Models

---
+++ Travel/Travel/Models/Trips.cs
@@ -0,0 +1,10 @@
+﻿namespace Travel.Models
+{
+    public class Trips
+    {
+        public string Destination { get; set; }
+        public DateOnly Date { get; set; }
+        public Users Organizer { get; set; }
+        public List<Users> Participants { get; set; }
+    }
+}
