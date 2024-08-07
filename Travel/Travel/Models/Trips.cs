00aa959049f38251b9a0034bfa091d36c29a0ab5
IvanMomchilov123 <vanko.momchilov@gmail.com>
2024-08-07 17:16:54 +0300

    Added IDs to Users and Trips. Changed dates and collections.

--- Travel/Travel/Models/Trips.cs
+++ Travel/Travel/Models/Trips.cs
@@ -2,9 +2,11 @@
 {
     public class Trips
     {
+        public int TripID { get; set; }
         public string Destination { get; set; }
-        public DateOnly Date { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
         public Users Organizer { get; set; }
-        public List<Users> Participants { get; set; }
+        public ICollection<Users> Participants { get; set; }
     }
 }
--- Travel/Travel/Models/Users.cs
+++ Travel/Travel/Models/Users.cs
@@ -2,9 +2,10 @@
 {
     public class Users
     {
+        public int UserID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public List<Trips> Trips { get; set; }
+        public ICollection<Trips> Trips { get; set; }
     }
 }
