e5895d3160eead1c8a80a668f35d1253e27e6dcf
IvanMomchilov123 <vanko.momchilov@gmail.com>
2024-08-09 11:47:26 +0300

    DbContext fix

--- Travel/Travel/Data/TravelDbContext.cs
+++ Travel/Travel/Data/TravelDbContext.cs
@@ -18,8 +18,9 @@ namespace Travel.Data
             modelBuilder.Entity<Trip>().HasKey(t => t.TripID);
 
             modelBuilder.Entity<TripParticipant>().HasKey(tp => new { tp.TripId, tp.UserId });
+
             modelBuilder.Entity<TripParticipant>().HasOne(tp => tp.Trip).WithMany(t => t.TripParticipants).HasForeignKey(tp => tp.TripId);
-            modelBuilder.Entity<TripParticipant>().HasOne(tp => tp.Trip).WithMany(t => t.TripParticipants).HasForeignKey(tp => tp.TripId);
+            modelBuilder.Entity<TripParticipant>().HasOne(tp => tp.User).WithMany(u => u.TripParticipants).HasForeignKey(tp => tp.User);
             modelBuilder.Entity<Trip>().HasOne(t => t.Organizer).WithMany(u => u.OrganizedTrips).HasForeignKey(t => t.OrganizerID);
 
         }
